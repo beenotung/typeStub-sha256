75f754643369c965e9f30096dac71da8cb9e288a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-07-03 12:21:21 +0800

    overloaded methods for better type support

--- index.ts
+++ index.ts
@@ -4,7 +4,31 @@ export type ByteArray = number[];
 export type Sha256Option = { asBytes: boolean } | { asString: boolean };
 
 export interface sha256 {
+    /**
+     * return Array{number} of length 32
+     * */
+    (data: string | ByteArray, option: { asBytes: true }): number[];
+
+    /**
+     * return Binary String
+     * */
+    (data: string | ByteArray, option: { asString: true }): string;
+
+    /**
+     * return string in hex of 64 chars
+     * */
     (data: string | ByteArray, option?: Sha256Option): string;
+
+
+    /**
+     * return Array{number} of length 32
+     * */
+    x2(data: string | ByteArray, option: { asBytes: true }): number[];
+
+    /**
+     * return Binary String
+     * */
+    x2(data: string | ByteArray, option: { asString: true }): string;
 
     x2(data: string | ByteArray, option?: Sha256Option): string;
 }
